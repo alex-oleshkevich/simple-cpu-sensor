ad423d2845c827af066ca57745e1186eaac2e859
alex.oleshkevich <alex.oleshkevich@muehlemann-popp.ch>
2012-03-23 18:27:05 +0300

    split methods into adapters

---
+++ contents/code/adapter/manager.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#   Copyright 2012 Alex Oleshkevich <alex.oleshkevich@gmail.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU Library General Public License as
+#   published by the Free Software Foundation; either version 2 or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#
+#   GNU General Public License for more details
+#
+#
+#   You should have received a copy of the GNU Library General Public
+#   License along with this program; if not, write to the
+#   Free Software Foundation, Inc.,
+#
+#   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+import sys
+from sysfs import *
+from acpi import *
+from sensors import *
+
+DEFAULT_ADAPTER = 'sysfs'
+
+class AdapterManager():
+	def __init__(self):
+		self.adapter = None
+		self.adapters = ['sysfs', 'acpi', 'sensors']
+	
+	def setAdapter(self, name):
+		adapter = getattr(sys.modules[__name__], name)
+		self.adapter = adapter()
+
+	def getAdapter(self):
+		return self.adapter
+
+	def getTemperature(self):
+		return self.adapter.getTemperature()
+
+	def getAvailableAdapters(self):
+		return self.adapters
--- contents/code/main.py
+++ contents/code/main.py
@@ -25,12 +25,13 @@ from PyQt4.QtGui import *
 from PyKDE4.kdecore import *
 from PyKDE4.kdeui import *
 from PyKDE4.kio import *
-from PyKDE4.solid import *
 from PyKDE4.plasma import Plasma
 from PyKDE4 import plasmascript
 from PyQt4 import QtCore
 
 import commands
+
+from adapter.manager import *
 
 # config tab
 from configwindow import *
@@ -43,23 +44,28 @@ from util import *
 class CPUTemp(plasmascript.Applet):
 	def __init__(self,parent, args=None):
 		plasmascript.Applet.__init__(self,parent)
+		self.adapter = AdapterManager()
+
+		self.availableAdapters = self.adapter.getAvailableAdapters()
 
 	def init(self):
-		self._name = str(self.package().metadata().pluginName());
+		self._name = str(self.package().metadata().pluginName())
 		self.layout = QGraphicsLinearLayout(Qt.Horizontal, self.applet)
 		self.setHasConfigurationInterface(True)
 		
 		self.label = Plasma.Label(self.applet)
-		self.label.setText("0")
+		self.label.setText("N\A")
 		self.layout.addItem(self.label)
 		self.applet.setLayout(self.layout)
-		self.resize(90, 20)
 
 		# Setup configuration
 		self.settings = Config(self)
 		self.color = self.settings.get('color', '#fff')
 		self.interval = int(self.settings.get('interval', 500))
-		self.method = self.settings.get('method', 'lm-sensors')
+		self.method = self.settings.get('method', 'sysfs')
+
+		print self.method
+		
 		self.overheat_level = int(self.settings.get('overheat_level', 80))
 		self.overheat_color = self.settings.get('overheat_color', '#f00')
 		self.units = self.settings.get('units', 'Celsius')
@@ -67,12 +73,24 @@ class CPUTemp(plasmascript.Applet):
 		# start timer
 		self.timer = QtCore.QTimer()
 		self.timer.setInterval(self.interval)
-		self.timer.start(1000)
-		QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.updateLabel)
 
-		# update temp label
-		self.updateLabel()
+		self.startPolling()
 
+	def startPolling(self):
+		try:
+			self.adapter.setAdapter(self.method)
+
+			self.timer.start(1000)
+			QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.updateLabel)
+
+			# update temp label
+			self.updateLabel()
+			self.showTooltip('')
+		except Exception as (strerror):
+			self.showTooltip(str(strerror))
+			self.label.setText('<font color="red">ERROR</font>')
+			return
+		
 	# ---------------------- configuration ------------------------#
 	def createConfigurationInterface(self, parent):
 		self.configpage = ConfigWindow(self, self.settings)
@@ -113,34 +131,36 @@ class CPUTemp(plasmascript.Applet):
 		self.settings.set('overheat_level', self.overheat_level)
 		self.settings.set('method', self.method)
 
-
 		# update timer
 		self.timer.setInterval(self.interval)
+
+		self.startPolling()
 			
 		print '[%s]: config accepted' % self._name
 
-		
 	def updateLabel(self):
-		t = -1
-		if (self.method == 'lm-sensors'):
-			sensor = commands.getoutput("sensors | grep temp1");
-			sensor = sensor[sensor.find("+") + 1 : sensor.find("C") - 2];
+		t = self.adapter.getTemperature()
 
-			t = int(float(sensor))
-		else:
-			try:
-				f = open('/proc/acpi/thermal_zone/THRM/temperature', 'r')
-				t = f.read()
-				t = int(t.replace('temperature:', '').replace('C', ''));
-			except:
-				print 'Unable to use %s method.' % self.method
-
-		if (t > self.overheat_level):
+		print t
+		print self.overheat_level
+		
+		if t > self.overheat_level:
+			print 'over'
 			self.color = self.overheat_color
 		else:
+			print self.color
 			self.color = self.settings.get('color', self.color)
 			
-		self.label.setText('<font color="' + self.color + '"><b>' + str(t) + '&deg; C</b></font>')
+		self.label.setText('<font style="color:' + self.color + '"><b>' + str(t) + '&deg; C</b></font>')
 
+	def showTooltip(self, text):
+			# create and set tooltip
+			tooltip = Plasma.ToolTipContent()
+			tooltip.setImage(KIcon(self.icon()))
+			tooltip.setSubText(text)
+			tooltip.setAutohide(False)
+			Plasma.ToolTipManager.self().setContent(self.applet, tooltip)
+			Plasma.ToolTipManager.self().registerWidget(self.applet)
+		
 def CreateApplet(parent):
 	return CPUTemp(parent)
